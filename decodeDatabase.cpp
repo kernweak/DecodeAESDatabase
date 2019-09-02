e94be0d50c7b6c79edcd0ea233f0514da5ed7bc9
kernweak <yangruiqiyr@163.com>
2019-09-02 11:54:10 +0800

    添加项目文件。

---
+++ decodeDatabase.cpp
@@ -0,0 +1,157 @@
+﻿// decodeDatabase.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+#include "pch.h"
+#include <iostream>
+#include <Windows.h>
+#include <openssl/rand.h>
+#include <openssl/evp.h>
+#include <openssl/aes.h>
+#include <openssl/hmac.h>
+
+using namespace std;
+
+#pragma comment(lib, "ssleay32.lib")
+#pragma comment(lib, "libeay32.lib")
+
+#if _MSC_VER>=1900
+#include "stdio.h" 
+_ACRTIMP_ALT FILE* __cdecl __acrt_iob_func(unsigned);
+#ifdef __cplusplus 
+extern "C"
+#endif 
+FILE* __cdecl __iob_func(unsigned i) {
+	return __acrt_iob_func(i);
+}
+#endif /* _MSC_VER>=1900 */
+
+
+
+#undef _UNICODE
+#define SQLITE_FILE_HEADER "SQLite format 3" 
+#define IV_SIZE 16
+#define HMAC_SHA1_SIZE 20
+#define KEY_SIZE 32
+
+#define SL3SIGNLEN 20
+
+
+#ifndef ANDROID_WECHAT
+#define DEFAULT_PAGESIZE 4096       //4048数据 + 16IV + 20 HMAC + 12
+#define DEFAULT_ITER 64000
+#else
+#define NO_USE_HMAC_SHA1
+#define DEFAULT_PAGESIZE 1024
+#define DEFAULT_ITER 4000
+#endif
+
+//pc端密码是经过OllyDbg得到的64位pass,是64位，不是网上传的32位，这里是个坑
+unsigned char pass[] = { 0x1C,0xD2,0x80,0x86,0xCE,0x2D,0x4A,0x5A,0x92,0xB3,0x14,0x93,0x2A,0x91,0x62,0x22,0x28,0x53,0x18,0x70,0x69,0xB4,0x4C,0xF2,0x8A,0x7E,0x1A,0xC5,0x0E,0xBC,0x90,0x1F };
+int Decryptdb();
+int main() {
+	Decryptdb();
+	return 0;
+}
+int Decryptdb() {
+
+	const char*  dbfilename = "Misc.db";
+	FILE* fpdb;
+	fopen_s(&fpdb, dbfilename, "rb+");
+	if (!fpdb) {
+		printf("打开文件错!");
+		getchar();
+		return 0;
+	}
+	fseek(fpdb, 0, SEEK_END);
+	long nFileSize = ftell(fpdb);
+	fseek(fpdb, 0, SEEK_SET);
+	unsigned char* pDbBuffer = new unsigned char[nFileSize];
+	fread(pDbBuffer, 1, nFileSize, fpdb);
+	fclose(fpdb);
+
+	unsigned char salt[16] = { 0 };
+	memcpy(salt, pDbBuffer, 16);
+
+#ifndef NO_USE_HMAC_SHA1
+	unsigned char mac_salt[16] = { 0 };
+	memcpy(mac_salt, salt, 16);
+	for (int i = 0; i < sizeof(salt); i++) {
+		mac_salt[i] ^= 0x3a;
+	}
+#endif
+
+	int reserve = IV_SIZE;      //校验码长度,PC端每4096字节有48字节
+#ifndef NO_USE_HMAC_SHA1
+	reserve += HMAC_SHA1_SIZE;
+#endif
+	reserve = ((reserve % AES_BLOCK_SIZE) == 0) ? reserve : ((reserve / AES_BLOCK_SIZE) + 1) * AES_BLOCK_SIZE;
+
+	unsigned char key[KEY_SIZE] = { 0 };
+	unsigned char mac_key[KEY_SIZE] = { 0 };
+
+	OpenSSL_add_all_algorithms();
+	PKCS5_PBKDF2_HMAC_SHA1((const char*)pass, sizeof(pass), salt, sizeof(salt), DEFAULT_ITER, sizeof(key), key);
+#ifndef NO_USE_HMAC_SHA1
+	//此处源码，怀凝可能有错，pass 数组才是密码
+	//PKCS5_PBKDF2_HMAC_SHA1((const char*)key, sizeof(key), mac_salt, sizeof(mac_salt), 2, sizeof(mac_key), mac_key);
+	PKCS5_PBKDF2_HMAC_SHA1((const char*)key, sizeof(key), mac_salt, sizeof(mac_salt), 2, sizeof(mac_key), mac_key);
+#endif
+
+	unsigned char* pTemp = pDbBuffer;
+	unsigned char pDecryptPerPageBuffer[DEFAULT_PAGESIZE];
+	int nPage = 1;
+	int offset = 16;
+	while (pTemp < pDbBuffer + nFileSize) {
+		printf("解密数据页:%d/%d \n", nPage, nFileSize / DEFAULT_PAGESIZE);
+
+#ifndef NO_USE_HMAC_SHA1
+		unsigned char hash_mac[HMAC_SHA1_SIZE] = { 0 };
+		unsigned int hash_len = 0;
+		HMAC_CTX hctx;
+		HMAC_CTX_init(&hctx);
+		HMAC_Init_ex(&hctx, mac_key, sizeof(mac_key), EVP_sha1(), NULL);
+		HMAC_Update(&hctx, pTemp + offset, DEFAULT_PAGESIZE - reserve - offset + IV_SIZE);
+		HMAC_Update(&hctx, (const unsigned char*)& nPage, sizeof(nPage));
+		HMAC_Final(&hctx, hash_mac, &hash_len);
+		HMAC_CTX_cleanup(&hctx);
+		if (0 != memcmp(hash_mac, pTemp + DEFAULT_PAGESIZE - reserve + IV_SIZE, sizeof(hash_mac))) {
+			printf("\n 哈希值错误! \n");
+			getchar();
+			return 0;
+		}
+#endif
+		//
+		if (nPage == 1) {
+			memcpy(pDecryptPerPageBuffer, SQLITE_FILE_HEADER, offset);
+		}
+
+		EVP_CIPHER_CTX* ectx = EVP_CIPHER_CTX_new();
+		EVP_CipherInit_ex(ectx, EVP_get_cipherbyname("aes-256-cbc"), NULL, NULL, NULL, 0);
+		EVP_CIPHER_CTX_set_padding(ectx, 0);
+		EVP_CipherInit_ex(ectx, NULL, NULL, key, pTemp + (DEFAULT_PAGESIZE - reserve), 0);
+
+		int nDecryptLen = 0;
+		int nTotal = 0;
+		EVP_CipherUpdate(ectx, pDecryptPerPageBuffer + offset, &nDecryptLen, pTemp + offset, DEFAULT_PAGESIZE - reserve - offset);
+		nTotal = nDecryptLen;
+		EVP_CipherFinal_ex(ectx, pDecryptPerPageBuffer + offset + nDecryptLen, &nDecryptLen);
+		nTotal += nDecryptLen;
+		EVP_CIPHER_CTX_free(ectx);
+
+		memcpy(pDecryptPerPageBuffer + DEFAULT_PAGESIZE - reserve, pTemp + DEFAULT_PAGESIZE - reserve, reserve);
+		char decFile[1024] = { 0 };
+		sprintf_s(decFile, "dec_%s", dbfilename);
+		FILE * fp;
+		fopen_s(&fp, decFile, "ab+");
+		{
+			fwrite(pDecryptPerPageBuffer, 1, DEFAULT_PAGESIZE, fp);
+			fclose(fp);
+		}
+
+		nPage++;
+		offset = 0;
+		pTemp += DEFAULT_PAGESIZE;
+	}
+	printf("\n 解密成功! \n");
+	system("pause");
+	return 0;
+}
